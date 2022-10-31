0f3a203da8d8d15c16341f2f8ad290700e01c8e4
riyad <87876277+riy17@users.noreply.github.com>
2022-10-31 17:57:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,47 @@ client.on("ready", () => {
   console.log(`✅ | Logged as ${client.user.tag}`);
 })
 //////////////////////////////////////
-client.on("message", async (message) => {
-if(message.channel.id != "1012444973448319077") return //ايدي روم
-if(message.author.send) {
-if (isNaN(message.content)) return message.delete()
-if(message.author.bot) return 
-message.react("✅") 
-let tax = Math.ceil(message.content * 20 / 19)
-message.channel.send(tax)
-}
-}) 
+client.on("message", async bader => {
+    let args = bader.content
+        .split(" ")
+        .slice(1)
+        .join(" ");
+    if (bader.author.bot) return;
+    if (!bader.guild) return;
+    if (!bader.content.startsWith(prefix)) return;
+    if (bader.content.toLowerCase().startsWith(prefix + "tax".toLowerCase())) {
+        let args2 = parseInt(args)
+        let tax = Math.floor(args2 * (20) / (19) + (1))
+        let tax2 = Math.floor(args2 * (20) / (19) + (1) - (args2))
+        let tax3 = Math.floor(tax2 * (20) / (19) + (1))
+        let tax4 = Math.floor(tax2 + tax3 + args2)
+        let embed1 = new Discord.MessageEmbed()
+            .setTitle(`**❌**`)
+            .setColor("#f04747")
+            .setDescription(`رقم :؟`)
+            .setFooter(`${client.user.username}`);
+        if (!args2) return bader.channel.send(embed1);
+        let embed2 = new Discord.MessageEmbed()
+            .setTitle(`**❌**`)
+            .setColor("#f04747")
+            .setDescription(`رقم :؟`)
+            .setFooter(`${client.user.username}`);
+        if (isNaN(args2)) return bader.channel.send(embed2);
+        if (args2 < 1) return bader.channel.send(3);
+        let embed4 = new Discord.MessageEmbed()
+            .setTitle(`التكلفة النهائية هي:`)
+            .setColor("#43b581")
+            .setDescription(`1`)
+            .setFooter(`${client.user.username}`);
+        if (args2 == 1) return bader.channel.send(embed4);
+        let taxbader = new Discord.MessageEmbed()
+            .setTitle(`الضريبه`)
+            .setColor("#43b581")
+            .setDescription(`الكريدت مع الضريبه غير شامل الوسيط : ${tax}\n  الكريدت شامل الوسيط : ${tax4}`)
+        bader.channel.send(taxbader);
+    }
+});
+
 
 ///
 client.login(process.env.token)
