88b874e64c8229531e64ba16f5a759550245c0ae
riyad <87876277+riy17@users.noreply.github.com>
2022-10-31 18:04:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,44 @@
-const Discord = require('discord.js');
-const { Client, Intents,MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
+const express = require('express');
 
-client.on("ready", () => {
-  console.log(`✅ | Logged as ${client.user.tag}`);
-})
-//////////////////////////////////////
-client.on("message", async bader => {
-    let args = bader.content
-        .split(" ")
-        .slice(1)
-        .join(" ");
-    if (bader.author.bot) return;
-    if (!bader.guild) return;
-    if (!bader.content.startsWith(prefix)) return;
-    if (bader.content.toLowerCase().startsWith("tax".toLowerCase())) {
-        let args2 = parseInt(args)
-        let tax = Math.floor(args2 * (20) / (19) + (1))
-        let tax2 = Math.floor(args2 * (20) / (19) + (1) - (args2))
-        let tax3 = Math.floor(tax2 * (20) / (19) + (1))
-        let tax4 = Math.floor(tax2 + tax3 + args2)
-        let embed1 = new Discord.MessageEmbed()
-            .setTitle(`**❌**`)
-            .setColor("#f04747")
-            .setDescription(`رقم :؟`)
-            .setFooter(`${client.user.username}`);
-        if (!args2) return bader.channel.send(embed1);
-        let embed2 = new Discord.MessageEmbed()
-            .setTitle(`**❌**`)
-            .setColor("#f04747")
-            .setDescription(`رقم :؟`)
-            .setFooter(`${client.user.username}`);
-        if (isNaN(args2)) return bader.channel.send(embed2);
-        if (args2 < 1) return bader.channel.send(3);
-        let embed4 = new Discord.MessageEmbed()
-            .setTitle(`التكلفة النهائية هي:`)
-            .setColor("#43b581")
-            .setDescription(`1`)
-            .setFooter(`${client.user.username}`);
-        if (args2 == 1) return bader.channel.send(embed4);
-        let taxbader = new Discord.MessageEmbed()
-            .setTitle(`الضريبه`)
-            .setColor("#43b581")
-            .setDescription(`الكريدت مع الضريبه غير شامل الوسيط : ${tax}\n  الكريدت شامل الوسيط : ${tax4}`)
-        bader.channel.send(taxbader);
-    }
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+    console.log('server started');
+});
+const Discord = require("discord.js");
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
 });
 
 
-///
+//Say Embed | ساي امبيد
+client.on('messageCreate', async message => {
+ if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (command === "say-embed") {
+    if (!args[0]) return await message.channel.send({content: `${prefix}say-embed <message>`});
+    let embed = new MessageEmbed()
+    .setDescription(`${args.join(" ")}`)
+    await message.channel.send({embeds: [embed]});
+  }
+})
+//https://ra3dstudio.com CopyRight Codes
+
+
+
+const prefix = "+"// bot prefix
 client.login(process.env.token)
 
+
+client.on("ready", async () => {
+    console.log(`"${client.user.username}" is ready`)//runing bot username
+
+})
