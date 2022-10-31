2d861dd516d3ae4ee8cced87c4e9705cd68b2bab
riyad <87876277+riy17@users.noreply.github.com>
2022-10-31 16:30:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,52 @@ client.on('messageCreate', message => {
   } 
 })
 
+client.on("message", async message => {
+///coded by : 
+
+   
+const prefix = "!";//برفكس حقك
+const devs = "525715719124680725";//اي دي حقك
+  let args = message.content
+    .split(" ")
+    .slice(1)
+    .join(" "); if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+ if (message.content.toLowerCase().startsWith(prefix + "tax".toLowerCase())) { 
+    let args2 = parseInt(args)
+    let tax = Math.floor(args2 * (20) / (19) + (1))
+    let errorembed3 = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**It Must be a number**`)
+    .setFooter(`${client.user.username}`);
+    if (!args2) return message.channel.send(errorembed3);
+    let errorembed2 = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**Must Be A Number**`)
+    .setFooter(`${client.user.username}`);
+    if (isNaN(args2)) return message.channel.send(errorembed2);
+    let errorembed = new Discord.MessageEmbed()
+    .setTitle(`**:information_source: Error**`)
+    .setColor("#f04747")
+    .setDescription(`**Must The Number Larger 1**`)
+    .setFooter(`${client.user.username}`);
+    if (args2 < 1) return message.channel.send(errorembed);
+    let embed3 = new Discord.MessageEmbed()
+    .setTitle(`**:notes: The Final Cost Is :**`)
+    .setColor("#43b581")
+    .setDescription(`1`)
+    .setFooter(`${client.user.username}`);
+    if (args2 == 1) return message.channel.send(embed3);
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`**:notes: The Final Cost Is :**`)
+    .setColor("#43b581")
+    .setDescription(`${tax}`)
+    .setFooter(`${client.user.username}`);
+    message.channel.send(embed);
+  })
 
 ///
 client.login(process.env.token)
